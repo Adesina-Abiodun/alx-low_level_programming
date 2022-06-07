663e9ebd923c1bbdae2e53d18311e356b5ec75f8
Adesina-Abiodun <Adesinaabiodunpaul@gmail.com>
2022-06-06 17:20:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include<unistd.h>
 /**
- * main - Entry point
+ * main - prints the standrad error sandwiched around the  standard outpu/t
  *
  * Return:1
      */
 int main(void)
-{int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-				s);
+{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }

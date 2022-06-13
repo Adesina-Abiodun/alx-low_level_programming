2a1010c79a2d5fac4d4e5f4ca97413bf1b9f1873
AdesinaAbiodun <adesinaabiodunpaul@gmail.com>
2022-06-13 12:17:43 +0100

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,19 @@
 #include "main.h"
+#include <string.h>
+
 /**
  * print_rev - prints a string in reverse order
- *@s: A pointer to an int that will be changed
- *
- *Return: void which means our answer is correct
+ * @s: string
  */
-
 void print_rev(char *s)
 {
-int i;
+	int i;
 
-i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
+	i = strlen(s);
 
-for (i = i - 1 ; i >= 0; i--)
-{
-_putchar (s[i]);
-}
-
-_putchar ('\n');
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
